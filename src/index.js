e5bc238e56dbf9cb694d89b3dc7976fb0859bab3
Carolina Castro <cariitto2426@gmail.com>
2024-08-24 12:22:04 -0600

    Proyecto Terminado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "./style.css";
+import "./styles.css";
 import { addTask, deleteTask, toggleTask } from "./task";
-import { renderTasks } from ".ui";
+import { renderTasks } from "./ui";
 
 document.addEventListener("DOMContentLoaded", () => {
     renderTasks();
@@ -9,6 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault();
 
       const taskInput = document.getElementById("task-input").value;
+      if(taskInput !== "") {
+        addTask(taskInput);
+        renderTasks();
+        //Limpiar el cuadro de texto
+        document.getElementById("task-input").value = "";
+      }
     });
+ 
+    document.getElementById("task-list").addEventListener("click", (e) => {
+      if(e.target.classList.contains("delete")) {
+          const taskId = e.target.parentElement.getAttribute("data-id");
+          deleteTask(taskId);
+          renderTasks();
+      }
 
+      if(e.target.classList.contains("toggle")) {
+        const taskId = e.target.parentElement.getAttribute("data-id");
+        toggleTask(taskId);
+        renderTasks();
+      }
+
+    });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js', //Punto de entrada de la aplicación = archivo de entrada donde empaquetará el webpack
   output: {
     filename: 'bundle.js',  //Nombre del archivo de salida
@@ -10,24 +11,26 @@ module.exports = {
     rules:[
         {
             test: /\.css$/, //Regex para identificar archivos CSS
-            use: ['style-loader','css-loader'], //Loaders para procesar archivos CSS
+            use: ['style-loader', 'css-loader'], //Loaders para procesar archivos CSS
         },
-        {
-           test: /\.js$/, //Regex para identificar archivos JS
-           exclude: /node_modules/, //Excluir la carpeta node_modules
-           use: {
-            loader: 'babel-loader',//Configurar loader para pasara JS moderno a JS más antiguo  para todos los navegadores
-            options:{
-                presets: ['@babel/preset-env'],
-            },
+       {
+        test: /\.js$/, // Regex para identificar archivos js
+        exclude: /node_modules/, // Excluir la carpeta node_modules
+        use:{
+           loader: 'babel-loader', // Configurar loader para  pasar JS moderno a JS antiguo para todos los navegadores
+           options: {
+            presets: ['@babel/preset-env']
            },
         },
+       },
     ],
   },
   devtool: 'source-map', //Generar source maps para facilitar la depuración
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'), //Carpeta principal del servidor
-    compress: true, // Habilitar la compresión gzip
+    static: {
+      directory: path.resolve(__dirname, 'dist'), //Carpeta principal del servidor
+     },
+      compress: true, // Habilitar la compresión gzip
     port: 9000, //Puerto del servidor de desarrollo
   },
 };
